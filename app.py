eaf7910e9e9753c91b1e830a3b47e3dc622a4f6a
DudiyMaksim <154374761+DudiyMaksim@users.noreply.github.com>
2024-03-01 22:00:11 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,19 @@
+from aiogram import Bot, Dispatcher
+from dotenv import load_dotenv, find_dotenv
+import os
+import asyncio
+from handlers.user_private import user_private_router
+
+
+load_dotenv(find_dotenv())
+
+
+bot = Bot(token=os.getenv('TOKEN'))
+
+dp = Dispatcher()
+dp.include_routers(user_private_router)
+
+async def main():
+    await dp.start_polling(bot)
+
+asyncio.run(main())
